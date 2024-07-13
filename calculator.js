e4b7c9f16815fb61d50204fe432b4fc2e9b123fd
oussamaaouam1 <oussama.aouam@gmail.com>
2024-07-13 15:38:51 +0100

    commit comments in calculator js

--- calculator.js
+++ calculator.js
@@ -1,19 +1,19 @@
- // Function to add two numbers
- function add(number1, number2) {
+ // Function for addition.
+function add(number1, number2) {
   return number1 + number2;
 }
 
-// Function to subtract two numbers
+// Function for subtraction.
 function subtract(number1, number2) {
   return number1 - number2;
 }
 
-// Function to multiply two numbers
+// Function for multiplication.
 function multiply(number1, number2) {
   return number1 * number2;
 }
 
-// Function to divide two numbers
+// Function to division.
 function divide(number1, number2) {
   if (number2 === 0) {
       return 'Error: Division by zero';
@@ -22,27 +22,28 @@ function divide(number1, number2) {
 }
 
 // Event listeners for buttons
+//=========================add=============================================
 document.getElementById('add').addEventListener('click', function() {
   const number1 = parseFloat(document.getElementById('number1').value) || 0;
   const number2 = parseFloat(document.getElementById('number2').value) || 0;
   const result = add(number1, number2);
   document.getElementById('calculation-result').textContent = result;
 });
-
+//=========================subtract========================================
 document.getElementById('subtract').addEventListener('click', function() {
   const number1 = parseFloat(document.getElementById('number1').value) || 0;
   const number2 = parseFloat(document.getElementById('number2').value) || 0;
   const result = subtract(number1, number2);
   document.getElementById('calculation-result').textContent = result;
 });
-
+//========================multiply=========================================
 document.getElementById('multiply').addEventListener('click', function() {
   const number1 = parseFloat(document.getElementById('number1').value) || 0;
   const number2 = parseFloat(document.getElementById('number2').value) || 0;
   const result = multiply(number1, number2);
   document.getElementById('calculation-result').textContent = result;
 });
-
+//========================divide===========================================
 document.getElementById('divide').addEventListener('click', function() {
   const number1 = parseFloat(document.getElementById('number1').value) || 0;
   const number2 = parseFloat(document.getElementById('number2').value) || 0;
